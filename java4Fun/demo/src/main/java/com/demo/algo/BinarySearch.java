e9763607360e21fac93af2bab27efd5dcb0f5eff
chrischeng <chrischeng021@gmail.com>
2023-02-09 09:51:39 +0000

    Add binarySearch

--- java4Fun/demo/src/main/java/com/demo/algo/BinarySearch.java
+++ java4Fun/demo/src/main/java/com/demo/algo/BinarySearch.java
@@ -1,5 +1,36 @@
 package main.java.com.demo.algo;
 
 public class BinarySearch {
-    
+    /**
+     * @param nums int array, source
+     * @param target int value, target
+     * @return -1 = not found, otherwise target value's index.
+     */
+    public static int search(int[] nums, int target) {
+        if(nums.length == 0) {
+            return -1;
+        }
+        return search(nums, target, 0, nums.length - 1);
+    }
+
+    private static int search(int[] nums, int target, int left, int right) {
+        if(left > right) {
+            return -1;
+        }
+        else if(left == right) {
+            return nums[left] == target ? left : -1;
+        }
+        else {
+            int mid = (left + right) / 2;
+            if(nums[mid] == target) {
+                return mid;
+            }
+            else if (nums[mid] > target) {
+                return search(nums,target, left, mid - 1);
+            }
+            else {
+                return search(nums, target, mid + 1, right);
+            }
+        }
+    }
 }
--- java4Fun/demo/src/test/java/com/demo/algo/BinarySearchTest.java
+++ java4Fun/demo/src/test/java/com/demo/algo/BinarySearchTest.java
@@ -1,9 +1,34 @@
 package test.java.com.demo.algo;
 
 import static org.junit.Assert.assertTrue;
-
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
-public class BinarySearchTest {
-    
+import main.java.com.demo.algo.BinarySearch;
+
+public class BinarySearchTest {    
+    @Test
+    public void testArray_01() {
+        assertEquals(0, BinarySearch.search(new int[] {0}, 0));
+    }
+
+    @Test
+    public void testArray_02() {
+        assertEquals(-1, BinarySearch.search(new int[] {0}, 3));
+    }
+
+    @Test
+    public void testArray_03() {
+        assertEquals(1, BinarySearch.search(new int[] {1, 2}, 2));
+    }
+
+    @Test
+    public void testArray_04() {
+        assertEquals(2, BinarySearch.search(new int[] {1, 2, 3}, 3));
+    }
+
+    @Test
+    public void testArray_05() {
+        assertEquals(1, BinarySearch.search(new int[] {1, 2, 3, 4}, 2));
+    }
 }
