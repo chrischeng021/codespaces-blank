9140aa28fbaaf7ea0acf0249c4e4a3c36567fae8
chrischeng <chrischeng021@gmail.com>
2023-02-09 07:16:31 +0000

    Re-construct project

---
+++ java4Fun/demo/src/main/java/com/demo/algo/BinarySearch.java
@@ -0,0 +1,5 @@
+package main.java.com.demo.algo;
+
+public class BinarySearch {
+    
+}
---
+++ java4Fun/demo/src/test/java/com/demo/algo/BinarySearchTest.java
@@ -0,0 +1,9 @@
+package test.java.com.demo.algo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class BinarySearchTest {
+    
+}
